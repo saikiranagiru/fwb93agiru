257692acd0ecabdac55fd0500529de5ed7a155b3
saikiranagiru <84036204+saikiranagiru@users.noreply.github.com>
2021-11-03 16:47:13 -0500

    added images

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ovenRouter = require('./routes/oven');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 
 
 var app = express();
@@ -26,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/oven', ovenRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 
 
